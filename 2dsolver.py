c1a4b4677d7c87ff5d99d850be74533a7aa1bf77
Unknown <niklas.eriksson@fgz.ch>
2018-12-03 22:14:15 +0100

    KGV update

--- 2dsolver.py
+++ 2dsolver.py
@@ -1,43 +1,32 @@
-A1 = [4, 3] # [15, 6] ---->  [15, 6]
-A2 = [1, -2] # [15, 25] ----> [0, 19] y = 19
-B1 = [2]
-B2 = [0]
+import fractions
 
-def kgV(zahlen):
-    mal = 1
-    while True:
-        vielfaches = zahlen[0] * mal
-        try:
-            for zahl in zahlen:
-                rest = (vielfaches % zahl)
-                if not rest: pass
-                else:
-                    raise
-            break
-        except: pass
-        mal += 1
-    return vielfaches
-def solve2d(a1, a2, b1, b2):
+A1 = [55, 34, 2231] # 55x + 34y = 2231
+A2 = [43, -212, -2321] # 43x + (-212y) = -2321
+
+
+def kgV(a, b): return abs(a*b) / fractions.gcd(a, b) if a and b else 0
+
+def solve2d(a1, a2):
 	# x werte der gleichung auf die gleiche Zahl bringen
 	old_a1 = a1
-	a2faktor = kgV([a1[0], a2[0]]) / a2[0]#old_a1[0]
-	a1faktor = kgV([a1[0], a2[0]]) / a1[0]
+	a2faktor = kgV(a1[0], a2[0]) / a2[0]#old_a1[0]
+	a1faktor = kgV(a1[0], a2[0]) / a1[0]
 	a1[0] *= a1faktor
 	a1[1] *= a1faktor
-	b1[0] *= a1faktor
+	a1[2] *= a1faktor
 	a2[0] *= a2faktor
 	a2[1] *= a2faktor
-	b2[0] *= a2faktor
+	a2[2] *= a2faktor
 	# Nun sind beide x werte die selben bei beiden gleichungen. Nun kann man die erste Gleichung von der zweiten abziehen
 	a2[0] = a2[0] - a1[0]
 	a2[1] = a2[1] - a1[1]
-	b2[0] = b2[0] - b1[0]
-	y = b2[0] / a2[1] # bei der zweiten gleichung ist jetzt nur noch die y variable. Wenn man ihn durch b2 teilt, erhält man y
+	a2[2] = a2[2] - a1[2]
+	y = a2[2] / a2[1] # bei der zweiten gleichung ist jetzt nur noch die y variable. Wenn man ihn durch b2 teilt, erhält man y
 	# nun kann man y in die erste gleichung einsetzen um x zu erhalten
 	gl1y = a1[1]*y
 	a1[1] = 0
-	b1[0] -= gl1y
-	x = b1[0] / a1[0]
+	a1[2] -= gl1y
+	x = a1[2] / a1[0]
 	print("X: " + str(x))
 	print("Y: " + str(y))
-solve2d(A1, A2, B1, B2)
+solve2d(A1, A2)
